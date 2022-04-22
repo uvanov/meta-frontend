6581c63ea512cbaa6e5d7d52ea100f388d965d70
uvanov <kirill.ivanov000@gmail.com>
2022-04-22 23:24:26 +0300

    Util changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // Import modules
 import React from 'react';
+import { Authentication } from './components/pages/authentication/Authentication';
 
 // Exports
 export const App = () => {
   return (
     <>
+      <Authentication/>
     </>
   );
 };
