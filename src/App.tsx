6b31a3dd2d257f81e52377491ece590f8c72b9ff
uvanov <kirill.ivanov000@gmail.com>
2022-04-22 20:57:06 +0300

    Remove all needless

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
 // Import modules
-import React, { useState } from 'react';
-import { Input } from './components/ui';
-import { ReactComponent as SomeIcon } from './assets/images/arrow-right.svg';
+import React from 'react';
+
+// Import local modules
 
 
 // Exports
 export const App = () => {
-
-  const [loginValue, setLoginValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-
-
   return (
-    <div style={ { maxWidth: '50%' } }>
-      <Input
-        Icon={ SomeIcon }
-        variant='text'
-        label='Логин'
-        placeholder='Ваш логин'
-        value={ loginValue }
-        onChange={ (event) => setLoginValue(event.target.value) }
-        isValid={ true }
-        invalidErrorText='Этот логин уже занят, пожалуйста, придумайте другой логин.'
-      />
-
-      <Input
-        Icon={ SomeIcon }
-        variant='password'
-        label='Пароль'
-        placeholder='Ваш пароль'
-        value={ passwordValue }
-        onChange={ (event) => setPasswordValue(event.target.value) }
-        isValid={ true }
-      />
-
-
-    </div>
+    <>
+    </>
   );
 };
 
