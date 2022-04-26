3658854b2a17d90e57a6449efda8ef5544c59432
uvanov <kirill.ivanov000@gmail.com>
2022-04-26 15:16:46 +0300

    Put CEF to window

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // Import modules
 import React from 'react';
+import { CEF } from './CEF';
 import { Authentication } from './components/pages/authentication/Authentication';
 
 // Exports
 export const App = () => {
+
+  window.CEF = CEF;
+
   return (
     <>
       <Authentication/>
