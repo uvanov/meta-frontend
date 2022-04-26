c19c5b1636b50fd785e064423231434f68748543
uvanov <kirill.ivanov000@gmail.com>
2022-04-26 14:26:18 +0300

    Add Form Validation to Authentication

--- src/components/pages/authentication/components/Recovery.jsx
+++ src/components/pages/authentication/components/Recovery.jsx
@@ -1,7 +1,9 @@
 // Import modules
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import Validator from 'validator/es/index';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useCheckForErrors } from '../../../../hooks';
 
 // Local modules
 import { FormWindow } from './FormWindow';
@@ -14,6 +16,9 @@ import {
 // Assets
 import { ReactComponent as EmailIcon } from '../../../../assets/images/authentication/email-icon.svg';
 import { ReactComponent as CodeIcon } from '../../../../assets/images/authentication/code-icon.svg';
+import { useAppDispatch } from '../../../../hooks/state';
+import { authenticationSlice } from '../../../../store/slices/AuthenticationSlice';
+
 
 // Styled Components
 const StyledFormWindow = styled(FormWindow)`
@@ -21,57 +26,122 @@ const StyledFormWindow = styled(FormWindow)`
   padding-bottom: 98px;
 `;
 
+// Constants
+const MAX_CODE_CHARACTERS = 6;
+const CODE_REGEX = /^[a-zA-Z0-9_.-]*$/;
+
+const VALIDATION_TEXT_ERRORS = {
+  EMAIL_REQUIRED: 'Введите почту',
+  EMAIL_WRONG_FORMAT: 'Неверный формат почты',
+  CODE_REQUIRED: 'Введите код',
+  WRONG_CODE_FORMAT: 'Код содержит только цифры',
+  MAX_CODE_SYMBOLS_ERROR: `Код не может быть длинее ${ MAX_CODE_CHARACTERS } символов`,
+};
+
+const submitHandler = (values) => {
+  global.mp.trigger('someEvent', JSON.stringify(values));
+}
+
 // Exports
 export const Recovery = () => {
 
-  const [emailValue, setEmailValue] = useState('');
-  const [codeValue, setCodeValue] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      code: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.EMAIL_REQUIRED)
+        .email(VALIDATION_TEXT_ERRORS.EMAIL_WRONG_FORMAT),
+      code: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.CODE_REQUIRED)
+        .max(MAX_CODE_CHARACTERS, VALIDATION_TEXT_ERRORS.MAX_CODE_SYMBOLS_ERROR)
+        .matches(CODE_REGEX, VALIDATION_TEXT_ERRORS.WRONG_CODE_FORMAT)
+    }),
+    onSubmit: (values) => {
+      submitHandler(values);
+    }
+  });
+
+  const [isEmailValid, emailErrorText] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'email',
+    interface: 'recovery'
+  });
+  const [isCodeValid, codeErrorText] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'code',
+    interface: 'recovery'
+  });
+
+  const dispatch = useAppDispatch();
+  const { setAuthenticationView } = authenticationSlice.actions;
+
+  const switchToAuthorization = () => {
+    dispatch(setAuthenticationView({ view: 'authorization' }));
+  };
 
   return (
     <StyledFormWindow
       title='ВОССТАНОВЛЕНИЕ'
       subtitle='ВНИМАТЕЛЬНО ВВОДИТЕ ЛИЧНЫЕ ДАННЫЕ, ОТВЕТСТВЕННОСТЬ ЗА ВАШ АККАУНТ НЕСЕТЕ ТОЛЬКО ВЫ.'
     >
-      <Flex
-        direction='column'
-        gap='10px'
+      <form
+        onSubmit={ formik.handleSubmit }
+        autoComplete='off'
       >
-        <Input
-          variant='text'
-          label='Почта'
-          placeholder='Ваша почта'
-          Icon={ EmailIcon }
-          fullWidth
-          isValid={ true }
-          value={ emailValue }
-          onChange={ event => setEmailValue(event.target.value) }
-        />
-        <Input
-          variant='text'
-          label='Код из письма'
-          placeholder='Код'
-          Icon={ CodeIcon }
-          fullWidth
-          isValid={ true }
-          value={ codeValue }
-          onChange={ event => setCodeValue(event.target.value) }
-        />
-        <Button
-          variant='danger'
-          large={ true }
-          fullWidth={ true }
-          onClick={ () => console.log() }
+        <Flex
+          direction='column'
+          gap='10px'
         >
-          ОТПРАВИТЬ КОД
-        </Button>
-        <Button
-          variant='default'
-          fullWidth={ true }
-          onClick={ () => console.log() }
-        >
-          НАЗАД
-        </Button>
-      </Flex>
+          <Input
+            id='email'
+            name='email'
+            variant='text'
+            label='Почта'
+            placeholder='Ваша почта'
+            value={ formik.values.email }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+            Icon={ EmailIcon }
+            fullWidth
+            isValid={ isEmailValid }
+            errorText={ emailErrorText }
+          />
+          <Input
+            id='code'
+            name='code'
+            variant='text'
+            label='Код из письма'
+            value={ formik.values.code }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+            placeholder='Код'
+            Icon={ CodeIcon }
+            fullWidth
+            isValid={ isCodeValid }
+            errorText={ codeErrorText }
+          />
+          <Button
+            type='submit'
+            variant='danger'
+            large={ true }
+            fullWidth={ true }
+          >
+            ОТПРАВИТЬ КОД
+          </Button>
+          <Button
+            variant='default'
+            fullWidth={ true }
+            onClick={ () => switchToAuthorization() }
+          >
+            НАЗАД
+          </Button>
+        </Flex>
+      </form>
     </StyledFormWindow>
   );
 };
--- src/components/pages/authentication/components/Registration.jsx
+++ src/components/pages/authentication/components/Registration.jsx
@@ -1,7 +1,7 @@
 // Import modules
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
-import Validator from 'validator/es/index';
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 // Local modules
 import { FormWindow } from './FormWindow';
@@ -10,6 +10,7 @@ import {
   Flex,
   Button
 } from '../../../ui';
+import { useCheckForErrors } from '../../../../hooks';
 
 // Assets
 import { ReactComponent as UserIcon } from '../../../../assets/images/authentication/authorization-button-icon.svg';
@@ -19,86 +20,194 @@ import { ReactComponent as DiscountIcon } from '../../../../assets/images/authen
 
 // Styled Components
 
+// Constants
+const US_LOCALES_SPECIAL_CHARACTERS_REGEX = /^[a-zA-Z$@$!%*?&#^-_. +]+$/;
+const US_LOCALES_REGEX = /^[A-Za-z]+$/;
+
+const MAX_SYMBOLS = 6;
+const MAX_PASSWORD_SYMBOLS = 30;
+
+const VALIDATION_TEXT_ERRORS = {
+  LOGIN_REQUIRED: 'Введите логин',
+  EMAIL_REQUIRED: 'Введите почту',
+  EMAIL_WRONG_FORMAT: 'Неверный формат почты',
+  PASSWORD_REQUIRED: 'Введите пароль',
+  PASSWORD_REPEAT_REQUIRED: 'Повторите свой пароль',
+  PASSWORDS_MUST_MATCH: 'Пароль должны совпадать',
+  MAX_SYMBOLS_ERROR: `Максимум ${ MAX_SYMBOLS } символов`,
+  MAX_PASSWORD_SYMBOLS_ERROR: `Серьёзно? У тебя пароль больше ${ MAX_PASSWORD_SYMBOLS } символов? `,
+  ONLY_US_LOCALES: 'Только английские буквы',
+  ONLY_US_AND_SPECIAL_SYMBOLS: 'Только английские буквы и спец. символы'
+};
+
+const submitHandler = (values) => {
+  global.mp.trigger('client.userRegistration', JSON.stringify(values))
+};
+
 // Exports
 export const Registration = () => {
 
-  const [loginValue, setLoginValue] = useState('');
-  const [emailValue, setEmailValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-  const [repeatPasswordValue, setRepeatPasswordValue] = useState('');
-  const [promoValue, setPromoValue] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      login: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+      promo: ''
+    },
+    // Схема может выглядеть чутка пиздецово, но я вынес все тексты в конфиг выше.
+    validationSchema: Yup.object({
+      login: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.LOGIN_REQUIRED)
+        .max(MAX_SYMBOLS, VALIDATION_TEXT_ERRORS.MAX_SYMBOLS_ERROR)
+        .matches(US_LOCALES_REGEX, VALIDATION_TEXT_ERRORS.ONLY_US_LOCALES),
+      email: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.EMAIL_REQUIRED)
+        .email(VALIDATION_TEXT_ERRORS.EMAIL_WRONG_FORMAT),
+      password: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.PASSWORD_REQUIRED)
+        .max(MAX_PASSWORD_SYMBOLS, VALIDATION_TEXT_ERRORS.MAX_PASSWORD_SYMBOLS_ERROR)
+        .matches(US_LOCALES_SPECIAL_CHARACTERS_REGEX, VALIDATION_TEXT_ERRORS.ONLY_US_AND_SPECIAL_SYMBOLS),
+      repeatPassword: Yup.string()
+        .required(VALIDATION_TEXT_ERRORS.PASSWORD_REPEAT_REQUIRED)
+        .oneOf([Yup.ref('password')], VALIDATION_TEXT_ERRORS.PASSWORDS_MUST_MATCH),
+      promo: Yup.string()
+        .max(MAX_SYMBOLS, VALIDATION_TEXT_ERRORS.MAX_SYMBOLS_ERROR)
+        .matches(US_LOCALES_REGEX, VALIDATION_TEXT_ERRORS.ONLY_US_LOCALES)
 
-  let values = {loginValue, emailValue, passwordValue, repeatPasswordValue, promoValue};
+    }),
+    onSubmit: (values) => {
+      submitHandler(values);
+    }
+  });
+
+  const [ isLoginValid, loginErrorText ] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'login',
+    interface: 'registration'
+  });
+  const [ isEmailValid, emailErrorText ] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'email',
+    interface: 'registration'
+  });
+  const [ isPasswordValid, passwordErrorText ] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'password',
+    interface: 'registration'
+  });
+  const [ isRepeatPasswordValid, repeatPasswordErrorText ] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'repeatPassword',
+    interface: 'registration'
+  });
+  const [ isPromoValid, promoErrorText ] = useCheckForErrors({
+    errors: formik.errors,
+    touched: formik.touched,
+    input: 'promo',
+    interface: 'registration'
+  });
+
+
+
 
   return (
     <FormWindow
       title='РЕГИСТРАЦИЯ'
       subtitle='ВНИМАТЕЛЬНО ВВОДИТЕ ЛИЧНЫЕ ДАННЫЕ, ОТВЕТСТВЕННОСТЬ ЗА ВАШ АККАУНТ НЕСЕТЕ ТОЛЬКО ВЫ.'
     >
-      <Flex
-        direction='column'
-        gap='10px'
+      <form
+        onSubmit={ formik.handleSubmit }
+        autoComplete='off'
       >
-        <Input
-          variant='text'
-          label='Логин'
-          placeholder='Ваш логин'
-          Icon={ UserIcon }
-          fullWidth
-          isValid={ true }
-          value={ loginValue }
-          onChange={ event => setLoginValue(event.target.value) }
-        />
-        <Input
-          variant='text'
-          label='Почта'
-          placeholder='Ваша почта'
-          Icon={ EmailIcon }
-          fullWidth
-          isValid={ true }
-          value={ emailValue }
-          onChange={ event => setEmailValue(event.target.value) }
-        />
-        <Input
-          variant='password'
-          label='Пароль'
-          placeholder='Ваш пароль'
-          Icon={ KeyIcon }
-          fullWidth
-          isValid={ true }
-          value={ passwordValue }
-          onChange={ event => setPasswordValue(event.target.value) }
-        />
-        <Input
-          variant='password'
-          label='Повторите пароль'
-          placeholder='Ваш пароль'
-          Icon={ KeyIcon }
-          fullWidth
-          isValid={ true }
-          value={ repeatPasswordValue }
-          onChange={ event => setRepeatPasswordValue(event.target.value) }
-        />
-        <Input
-          variant='text'
-          label='Промокод'
-          placeholder='Ваш промокод'
-          Icon={ DiscountIcon }
-          fullWidth
-          isValid={ true }
-          value={ promoValue }
-          onChange={ event => setPromoValue(event.target.value) }
-          invalidErrorText='Немного текста'
-        />
-        <Button
-          variant='danger'
-          large={ true }
-          fullWidth={ true }
-          onClick={ () => console.log(values) }
+        <Flex
+          direction='column'
+          gap='10px'
         >
-          ЗАРЕГИСТРИРОВАТЬСЯ
-        </Button>
-      </Flex>
+          <Input
+            id='login'
+            name='login'
+            variant='text'
+            label='Логин'
+            value={ formik.values.login }
+            placeholder='Ваш логин'
+            Icon={ UserIcon }
+            fullWidth
+            isValid={ isLoginValid }
+            errorText={ loginErrorText }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+          />
+          <Input
+            id='email'
+            name='email'
+            variant='text'
+            label='Почта'
+            placeholder='Ваша почта'
+            Icon={ EmailIcon }
+            fullWidth
+            isValid={ isEmailValid }
+            errorText={ emailErrorText }
+            value={ formik.values.email }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+          />
+          <Input
+            id='password'
+            name='password'
+            variant='password'
+            label='Пароль'
+            placeholder='Ваш пароль'
+            Icon={ KeyIcon }
+            fullWidth
+            isValid={ isPasswordValid }
+            errorText={ passwordErrorText }
+            value={ formik.values.password }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+          />
+          <Input
+            id='repeatPassword'
+            name='repeatPassword'
+            variant='password'
+            label='Повторите пароль'
+            placeholder='Ваш пароль'
+            Icon={ KeyIcon }
+            fullWidth
+            isValid={ isRepeatPasswordValid }
+            errorText={ repeatPasswordErrorText }
+            value={ formik.values.repeatPassword }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+          />
+          <Input
+            id='promo'
+            name='promo'
+            variant='text'
+            label='Промокод'
+            placeholder='Ваш промокод'
+            Icon={ DiscountIcon }
+            fullWidth
+            isValid={ isPromoValid }
+            errorText={ promoErrorText }
+            value={ formik.values.promo }
+            onChange={ formik.handleChange }
+            onBlur={ formik.handleBlur }
+          />
+          <Button
+            type='submit'
+            variant='danger'
+            large={ true }
+            fullWidth={ true }
+          >
+            ЗАРЕГИСТРИРОВАТЬСЯ
+          </Button>
+        </Flex>
+      </form>
     </FormWindow>
   );
 };
