5ee60cb0a7aeb7ef3c571e1e69471115e5ad5964
uvanov <kirill.ivanov000@gmail.com>
2022-04-26 14:28:28 +0300

    Create actions to change Authentication errors texts

--- src/store/slices/AuthenticationSlice.ts
+++ src/store/slices/AuthenticationSlice.ts
@@ -4,10 +4,53 @@ import {
   PayloadAction
 } from '@reduxjs/toolkit';
 
+// Types
+type ViewType = 'registration' | 'authorization' | 'recovery';
+
 // Initial state
 const initialState = {
-  isShown: true
+  isShown: true,
+  view: 'registration',
+  inputs: {
+    authorization: {
+      login: {
+        errorText: ''
+      },
+      password: {
+        errorText: ''
+      }
+    },
+    registration: {
+      login: {
+        errorText: ''
+      },
+      email: {
+        errorText: ''
+      },
+      password: {
+        errorText: ''
+      },
+      repeatPassword: {
+        errorText: ''
+      },
+      promo: {
+        errorText: ''
+      }
+    },
+    recovery: {
+      email: {
+        errorText: ''
+      },
+      code: {
+        errorText: ''
+      }
+    }
+  }
 };
+
+type AuthorizationFieldType = keyof typeof initialState.inputs.authorization;
+type RegistrationFieldType = keyof typeof initialState.inputs.registration;
+type RecoveryFieldType = keyof typeof initialState.inputs.recovery;
 
 // Init Authentication Slice
 export const authenticationSlice = createSlice({
@@ -16,6 +59,19 @@ export const authenticationSlice = createSlice({
   reducers: {
     setAuthenticationVisibility(state, action: PayloadAction<{ visibility: boolean }>) {
       state.isShown = action.payload.visibility;
+    },
+    setAuthenticationView(state, action: PayloadAction<{ view: ViewType }>){
+      console.log(1)
+      state.view = action.payload.view;
+    },
+    setAuthorizationError(state, action: PayloadAction<{ field: AuthorizationFieldType, value: string }>){
+      state.inputs.authorization[action.payload.field].errorText = action.payload.field;
+    },
+    setRegistrationError(state, action: PayloadAction<{ field: RegistrationFieldType, value: string }>){
+      state.inputs.registration[action.payload.field].errorText = action.payload.field;
+    },
+    setRecoveryError(state, action: PayloadAction<{ field: RecoveryFieldType, value: string }>){
+      state.inputs.recovery[action.payload.field].errorText = action.payload.field;
     }
   }
 });
