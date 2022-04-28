fb9b77154b20e0726d872e08865a919286453b76
uvanov <kirill.ivanov000@gmail.com>
2022-04-28 15:21:18 +0300

    add numberWithSpaces util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,7 @@ export function addHexAlpha(color: string, opacity: number) {
   let _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
   return color + _opacity.toString(16).toUpperCase();
 };
+
+export function numberWithSpaces(number: number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
