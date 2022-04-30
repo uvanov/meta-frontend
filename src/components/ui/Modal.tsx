a9008f1be94420672bbdaea7d8999113cfb103e9
uvanov <kirill.ivanov000@gmail.com>
2022-04-30 20:09:22 +0300

    Modal Components Layout

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,51 +1,94 @@
 // Import modules
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 // Local modules
-import { addHexAlpha } from "@lib/utils";
+import {addHexAlpha} from '@lib/utils';
 import {
   Flex,
+  MarginContainer,
   Typography
-} from "@ui/index";
-import {useAppSelector} from "@hooks/state";
+} from '@ui/index';
+import {useAppSelector} from '@hooks/state';
 
 // Styled Components
-const ModalBackground = styled(Flex)`
+const ModalBackground = styled(Flex, {
+  shouldForwardProp(prop) {
+    return prop !== 'isModalShown';
+  }
+})<Omit<ModalProps, 'title'>>`
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  z-index: 1000;
-  background-color: ${({ theme }) => addHexAlpha(theme.palette.black, 0.8)};
+  
+  background-color: ${({theme}) => addHexAlpha(theme.palette.black, 0.8)};
+  
+  transition: opacity .3s;
+  opacity: 0;
+  z-index: -1000;
+  
+  ${({ isModalShown }) => isModalShown && css`
+    opacity: 1;
+    z-index: 1000;
+  `};
 `;
 
 const StyledModal = styled(Flex)`
-  background: linear-gradient(${({ theme }) => theme.palette.bluegray} ${({ theme }) => addHexAlpha(theme.palette.bluegray, 0.1)});
-`
+  padding: 29px 90px 32px;
+  border-radius: 38px;
+  background: linear-gradient(180deg, ${({theme}) => theme.palette.bluegray},
+                                      ${({theme}) => addHexAlpha(theme.palette.bluegray, 0.1)});
+`;
 
 // Types
 interface ModalProps {
   title: string;
-  subtitle: string | React.ReactNode;
   isModalShown: boolean;
 }
 
 // Exports
-export const Modal: React.FC<ModalProps> = ({ children, title, subtitle, isModalShown }) => (
-  <>
+export const Modal: React.FC<ModalProps> = (
   {
-    isModalShown && (
+    children,
+    title,
+    isModalShown,
+    ...remainingProps
+
+  }) => {
+
+
+  return (
+    <>
+
       <ModalBackground
+        direction='column'
         justifyContent='center'
         alignItems='center'
+        isModalShown={ isModalShown }
       >
-        <StyledModal>
-          { children }
+
+        <StyledModal
+          direction='column'
+          justifyContent='center'
+          alignItems='center'
+          { ...remainingProps }
+        >
+          <Typography variant='title' color='gray'>
+            {title}
+          </Typography>
+
+          <MarginContainer
+            top='16px'
+            bottom='30px'
+          >
+            {children}
+          </MarginContainer>
         </StyledModal>
       </ModalBackground>
-    )
-  }
-  </>
-);
+
+    </>
+  )
+};
