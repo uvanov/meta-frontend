e3f69b8642bd026d42a96573f27807a8e13d20b4
uvanov <kirill.ivanov000@gmail.com>
2022-04-30 20:15:44 +0300

    lint fixes

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -4,13 +4,12 @@ import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
 // Local modules
-import {addHexAlpha} from '@lib/utils';
+import { addHexAlpha } from '@lib/utils';
 import {
   Flex,
   MarginContainer,
   Typography
 } from '@ui/index';
-import {useAppSelector} from '@hooks/state';
 
 // Styled Components
 const ModalBackground = styled(Flex, {
@@ -24,7 +23,7 @@ const ModalBackground = styled(Flex, {
   bottom: 0;
   left: 0;
   
-  background-color: ${({theme}) => addHexAlpha(theme.palette.black, 0.8)};
+  background-color: ${({ theme }) => addHexAlpha(theme.palette.black, 0.8)};
   
   transition: opacity .3s;
   opacity: 0;
@@ -39,8 +38,8 @@ const ModalBackground = styled(Flex, {
 const StyledModal = styled(Flex)`
   padding: 29px 90px 32px;
   border-radius: 38px;
-  background: linear-gradient(180deg, ${({theme}) => theme.palette.bluegray},
-                                      ${({theme}) => addHexAlpha(theme.palette.bluegray, 0.1)});
+  background: linear-gradient(180deg, ${({ theme }) => theme.palette.bluegray},
+                                      ${({ theme }) => addHexAlpha(theme.palette.bluegray, 0.1)});
 `;
 
 // Types
@@ -61,34 +60,29 @@ export const Modal: React.FC<ModalProps> = (
 
 
   return (
-    <>
-
-      <ModalBackground
+    <ModalBackground
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      isModalShown={ isModalShown }
+    >
+      <StyledModal
         direction='column'
         justifyContent='center'
         alignItems='center'
-        isModalShown={ isModalShown }
+        { ...remainingProps }
       >
+        <Typography variant='title' color='gray'>
+          { title }
+        </Typography>
 
-        <StyledModal
-          direction='column'
-          justifyContent='center'
-          alignItems='center'
-          { ...remainingProps }
+        <MarginContainer
+          top='16px'
+          bottom='30px'
         >
-          <Typography variant='title' color='gray'>
-            {title}
-          </Typography>
-
-          <MarginContainer
-            top='16px'
-            bottom='30px'
-          >
-            {children}
-          </MarginContainer>
-        </StyledModal>
-      </ModalBackground>
-
-    </>
-  )
+          { children }
+        </MarginContainer>
+      </StyledModal>
+    </ModalBackground>
+  );
 };
