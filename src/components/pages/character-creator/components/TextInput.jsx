602e058768e30c889abe0c4c5d0eb3c71ec009e7
uvanov <kirill.ivanov000@gmail.com>
2022-05-04 17:41:09 +0300

    TextInput Layout / Add valid mark

--- src/components/pages/character-creator/components/TextInput.jsx
+++ src/components/pages/character-creator/components/TextInput.jsx
@@ -1,6 +1,7 @@
 // Import modules
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 // Local modules
 import { addHexAlpha } from '@lib/utils';
@@ -10,47 +11,73 @@ import {
   Typography
 } from '@ui/index';
 
+// Assets
+import { ReactComponent as CheckIcon } from '@images/icons/check-icon.svg';
+import { ReactComponent as CrossIcon } from '@images/icons/cross-icon.svg';
+
 
 // Styled Components
 const InputWrapper = styled(Flex)`
   padding: 20px 28px;
+  border-radius: 9px;
   background: ${ ({ theme }) => addHexAlpha(theme.palette.bluegray, 0.5) };
-  width: fit-content;
 `;
 
 const StyledTextInput = styled.input`
+  border: 0;
+  outline: 0;
+  background-color: transparent;
   color: ${({ theme }) => theme.palette.white};
   font-size: ${({ theme }) => theme.typography.size.middle};
   
   &::placeholder {
     color: ${({ theme }) => theme.palette.gray};
+    font-weight: 500;
   }
 `;
 
-const ValidationMark = styled(Flex)`
+const ValidationFlex = styled(Flex)`
+  grid-column: 2 / 3;
+  grid-row: 1 / 3;
+`
+
+const ValidationMark = styled(Flex, {
+  shouldForwardProp(prop){
+    return prop !== 'valid';
+  }
+})`
   width: 26px;
   height: 26px;
   border-radius: 100%;
+  
+  transition: background-color .3s, box-shadow .3s;
+  
+  ${({ valid }) => valid === true
+    ? css`background: #88F861; box-shadow: 0px 0px 67px #88F861;`
+    : css`background: #F86161; box-shadow: 0px 0px 67px #F86161;`
+  }
 `;
 
 // Exports
 export const TextInput = (
   {
-    label,
+    label = 'Input label',
     value,
+    placeholder = 'Placeholder',
     onChange,
-    valid
+    valid, // null - пустота, false - не валид, true - валид
+    ...remainingProps
   }) => {
 
 
 
   return (
     <InputWrapper
-      justifyContent='center'
       alignItems='center'
     >
       <Grid
         columns={ 2 }
+        fullWidth
       >
         <Flex
           direction='column'
@@ -61,16 +88,29 @@ export const TextInput = (
           </Typography>
           <StyledTextInput
             type='text'
+            placeholder={ placeholder }
             value={ value }
             onChange={ onChange }
+            { ...remainingProps }
           />
         </Flex>
-        <Flex
+        <ValidationFlex
           justifyContent='flex-end'
-          alignItems='flex-end'
+          alignItems='center'
         >
-          lox
-        </Flex>
+          {
+            valid !== null && (
+              <ValidationMark
+                justifyContent='center'
+                alignItems='center'
+                valid={ valid }
+              >
+
+                { valid ? <CheckIcon/> : <CrossIcon/> }
+              </ValidationMark>
+            )
+          }
+        </ValidationFlex>
       </Grid>
     </InputWrapper>
   );
