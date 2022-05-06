b6b74c2a118c99e0ee9e892be76a9a9e3cf300cb
uvanov <kirill.ivanov000@gmail.com>
2022-05-06 12:52:40 +0300

    CharacterCreator wip

--- src/components/pages/character-creator/CharacterCreator.jsx
+++ src/components/pages/character-creator/CharacterCreator.jsx
@@ -1,15 +1,21 @@
 // Import modules
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+import { useFormik } from 'formik';
 
 // Local modules
 import {
+  Flex,
   Typography,
   Grid,
   MarginContainer
 } from '@ui/index';
-import { TabSwitchButton } from './components/TabSwitchButton';
+import { GradientButton } from './components/GradientButton';
+import { Dna } from './components/Dna';
+import { Skin } from './components/Skin';
+import { Face } from './components/Face';
+import { FacialStructure } from './components/FacialStructure';
+import { Hair } from './components/Hair';
 
 // Assets
 import { ReactComponent as DnaIcon } from '@images/character-creator/dna-icon.svg';
@@ -18,13 +24,19 @@ import { ReactComponent as FaceIcon } from '@images/character-creator/face-icon.
 import { ReactComponent as FacialStructureIcon } from '@images/character-creator/facial-structure-icon.svg';
 import { ReactComponent as HairIcon } from '@images/character-creator/hair-icon.svg';
 
+
 // Styled Components
 const CharacterCreatorWrapper = styled.div`
   width: 100%;
   height: 100%;
   padding: 59px 76px;
+  box-sizing: border-box;
   
   background-color: #393f47;
+`;
+
+const Container = styled(MarginContainer)`
+  max-width: 480px;
 `;
 
 const Title = styled(Typography)`
@@ -42,7 +54,7 @@ const Title = styled(Typography)`
     content: 'СОЗДАНИЕ ПЕРСОНАЖА';
     color: transparent;
     -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: ${({ theme }) => theme.palette.gray};
+    -webkit-text-stroke-color: ${ ({ theme }) => theme.palette.gray };
     left: 5%;
     
     transform: scale(1.2) translateZ(-1px);
@@ -51,59 +63,92 @@ const Title = styled(Typography)`
   }
 `;
 
-
 // Exports
 export const CharacterCreator = () => {
 
-  const [isActive, setActive] = useState(false);
+
+  const [val, setVal] = useState(0);
+  const [formState, setFormState] = useState({
+    name: '',
+    surname: '',
+    motherIndex: 0,
+    fatherIndex: 0,
+    motherSimilarity: 0,
+    fatherSimilarity: 0,
+    skinDefect: 0,
+    skinAging: 0,
+    skinType: 0,
+    noseWidth: 0,
+    noseHeight: 0,
+    noseLength: 0,
+    noseBridge: 0,
+    eyeSection: 0,
+    lipsSize: 0,
+    jawWidth: 0,
+    jawHeight: 0,
+    chinLength: 0,
+    chinJut: 0,
+    chinWidth: 0,
+    chinShape: 0,
+    hair: 0,
+    eyebrows: 0,
+    facialHair: 0
+  });
 
   return (
     <CharacterCreatorWrapper>
-      <Title variant='title'>
-        СОЗДАНИЕ <br/> ПЕРСОНАЖА
-      </Title>
-      <MarginContainer
-        top='23px'
-        bottom='23px'
-      >
-        <Grid
-          columns={ 5 }
-          columnWidth='83px'
-          columnGap={ 7 }
+      <Container>
+        <Title variant='title'>
+          СОЗДАНИЕ <br/> ПЕРСОНАЖА
+        </Title>
+        <MarginContainer
+          top='23px'
+          bottom='23px'
         >
-          <TabSwitchButton
-            active={ isActive }
-            Icon={ DnaIcon }
+          <Grid
+            columns={ 5 }
+            columnGap={ 7 }
           >
-            ДНК
-          </TabSwitchButton>
-          <TabSwitchButton
-            active={ isActive }
-            Icon={ AppearanceIcon }
-          >
-            Облик
-          </TabSwitchButton>
-          <TabSwitchButton
-            active={ isActive }
-            Icon={ FaceIcon }
-          >
-            Лицо
-          </TabSwitchButton>
-          <TabSwitchButton
-            active={ isActive }
-            Icon={ FacialStructureIcon }
-          >
-            Строение
-            лица
-          </TabSwitchButton>
-          <TabSwitchButton
-            active={ isActive }
-            Icon={ HairIcon }
-          >
-            Волосы
-          </TabSwitchButton>
-        </Grid>
-      </MarginContainer>
+            <GradientButton
+              active={ true }
+              Icon={ DnaIcon }
+            >
+              ДНК
+            </GradientButton>
+            <GradientButton
+              active={ true }
+              Icon={ AppearanceIcon }
+            >
+              Облик
+            </GradientButton>
+            <GradientButton
+              active={ true }
+              Icon={ FaceIcon }
+            >
+              Лицо
+            </GradientButton>
+            <GradientButton
+              active={ true }
+              Icon={ FacialStructureIcon }
+            >
+              Строение
+              лица
+            </GradientButton>
+            <GradientButton
+              active={ true }
+              Icon={ HairIcon }
+            >
+              Волосы
+            </GradientButton>
+          </Grid>
+        </MarginContainer>
+        <Flex
+          direction='column'
+          gap='10px'
+        >
+          <Hair state={ formState } setState={ setFormState }/>
+        </Flex>
+      </Container>
     </CharacterCreatorWrapper>
   );
 };
