12e9743e17a57dc5ce18f20acb26c47a438908ef
uvanov <kirill.ivanov000@gmail.com>
2022-05-06 17:53:42 +0300

    Add randomize-button / switch tab handler / conditional visibility

--- src/components/pages/character-creator/CharacterCreator.jsx
+++ src/components/pages/character-creator/CharacterCreator.jsx
@@ -1,14 +1,13 @@
 // Import modules
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { useFormik } from 'formik';
 
 // Local modules
 import {
   Flex,
   Typography,
   Grid,
-  MarginContainer
+  MarginContainer, Button
 } from '@ui/index';
 import { GradientButton } from './components/GradientButton';
 import { Dna } from './components/Dna';
@@ -16,6 +15,8 @@ import { Skin } from './components/Skin';
 import { Face } from './components/Face';
 import { FacialStructure } from './components/FacialStructure';
 import { Hair } from './components/Hair';
+import { useAppSelector } from '@hooks/state';
+import { getRandomFromRange } from '@lib/utils';
 
 // Assets
 import { ReactComponent as DnaIcon } from '@images/character-creator/dna-icon.svg';
@@ -23,6 +24,7 @@ import { ReactComponent as AppearanceIcon } from '@images/character-creator/appe
 import { ReactComponent as FaceIcon } from '@images/character-creator/face-icon.svg';
 import { ReactComponent as FacialStructureIcon } from '@images/character-creator/facial-structure-icon.svg';
 import { ReactComponent as HairIcon } from '@images/character-creator/hair-icon.svg';
+import { ReactComponent as RandomizeIcon } from '@images/icons/randomize-icon.svg';
 
 
 // Styled Components
@@ -31,11 +33,11 @@ const CharacterCreatorWrapper = styled.div`
   height: 100%;
   padding: 59px 76px;
   box-sizing: border-box;
-  
   background-color: #393f47;
 `;
 
-const Container = styled(MarginContainer)`
+const Container = styled(Flex)`
+  height: 100%;
   max-width: 480px;
 `;
 
@@ -63,11 +65,22 @@ const Title = styled(Typography)`
   }
 `;
 
+const RandomizeButton = styled(Flex)`
+  min-width: 75px;
+  height: 75px;
+  border-radius: 9px;
+  background: linear-gradient(
+      138.31deg, 
+      ${({ theme }) => theme.palette.red} -55%, 
+      ${({ theme }) => theme.palette.orange} 206.03%
+  );
+  cursor: pointer;
+`
+
 // Exports
 export const CharacterCreator = () => {
 
-
-  const [val, setVal] = useState(0);
+  // TODO: Переделать все компоненты нахуй. Перевести этот стейт на useReducer, перенести все страницы формы на диспатчи.
   const [formState, setFormState] = useState({
     name: '',
     surname: '',
@@ -94,61 +107,146 @@ export const CharacterCreator = () => {
     eyebrows: 0,
     facialHair: 0
   });
+  const [activeTab, setActiveTab] = useState('dna');
+  const isShown = useAppSelector(state => state.characterCreator.isShown);
+
+  const randomizeFormData = () => {
+
+    let randomizedData = {
+      motherIndex: getRandomFromRange(0, MOTHER_INDEX_MAX, true),
+      fatherIndex: getRandomFromRange(0, FATHER_INDEX_MAX, true),
+      motherSimilarity: getRandomFromRange(0, PARENT_SIMILARITY_MAX),
+      fatherSimilarity: getRandomFromRange(0, PARENT_SIMILARITY_MAX),
+      skinDefect: getRandomFromRange(0, DEFECTS_MAX, true),
+      skinAging: getRandomFromRange(0, AGING_MAX, true),
+      skinType: getRandomFromRange(0, TYPES_MAX, true),
+      noseWidth: getRandomFromRange(0, FACE_MAX),
+      noseHeight: getRandomFromRange(0, FACE_MAX),
+      noseLength: getRandomFromRange(0, FACE_MAX),
+      noseBridge: getRandomFromRange(0, FACE_MAX),
+      eyeSection: getRandomFromRange(0, FACE_MAX),
+      lipsSize: getRandomFromRange(0, FACE_MAX),
+      jawHeight: getRandomFromRange(0, FACE_MAX),
+      jawWidth: getRandomFromRange(0, FACE_MAX),
+      chinWidth: getRandomFromRange(0, FACE_MAX),
+      chinLength: getRandomFromRange(0, FACE_MAX),
+      chinJut: getRandomFromRange(0, FACE_MAX),
+      chinShape: getRandomFromRange(0, FACE_MAX),
+      hair: getRandomFromRange(0, HAIR_MAX, true),
+      eyebrows: getRandomFromRange(0, HAIR_MAX, true),
+      facialHair: getRandomFromRange(0, HAIR_MAX, true)
+    };
+    setFormState(randomizedData);
+    global.mp.trigger('client.randomCharacter',  JSON.stringify(randomizedData));
+  }
+  const changeTab = (tabName, tabIndex) => {
+    setActiveTab(tabName);
+    global.mp.trigger('client.changeTab', tabIndex);
+  };
+
+  const MOTHER_INDEX_MAX = useAppSelector(state => state.characterCreator.values.dna.mother.max);
+  const FATHER_INDEX_MAX = useAppSelector(state => state.characterCreator.values.dna.father.max);
+  const PARENT_SIMILARITY_MAX = useAppSelector(state => state.characterCreator.values.dna.parentSimilarity.max);
+  const AGING_MAX = useAppSelector(state => state.characterCreator.values.skin.aging.max);
+  const DEFECTS_MAX = useAppSelector(state => state.characterCreator.values.skin.defects.max);
+  const TYPES_MAX = useAppSelector(state => state.characterCreator.values.skin.types.max);
+  const FACE_MAX = useAppSelector(state => state.characterCreator.values.face.max);
+  const HAIR_MAX = useAppSelector(state => state.characterCreator.values.hair.max);
+
+  const FORM_PAGES_MAP = {
+    dna: <Dna state={ formState } setState={ setFormState }/>,
+    skin: <Skin state={ formState } setState={ setFormState }/>,
+    face: <Face state={ formState } setState={ setFormState }/>,
+    facialStructure: <FacialStructure state={ formState } setState={ setFormState }/>,
+    hair: <Hair state={ formState } setState={ setFormState }/>,
+  }
 
   return (
-    <CharacterCreatorWrapper>
-      <Container>
-        <Title variant='title'>
-          СОЗДАНИЕ <br/> ПЕРСОНАЖА
-        </Title>
-        <MarginContainer
-          top='23px'
-          bottom='23px'
-        >
-          <Grid
-            columns={ 5 }
-            columnGap={ 7 }
-          >
-            <GradientButton
-              active={ true }
-              Icon={ DnaIcon }
+    <>
+      {
+        isShown && (
+          <CharacterCreatorWrapper>
+            <Container
+              direction='column'
+              justifyContent='space-between'
+              gap='17px'
             >
-              ДНК
-            </GradientButton>
-            <GradientButton
-              active={ true }
-              Icon={ AppearanceIcon }
-            >
-              Облик
-            </GradientButton>
-            <GradientButton
-              active={ true }
-              Icon={ FaceIcon }
-            >
-              Лицо
-            </GradientButton>
-            <GradientButton
-              active={ true }
-              Icon={ FacialStructureIcon }
-            >
-              Строение
-              лица
-            </GradientButton>
-            <GradientButton
-              active={ true }
-              Icon={ HairIcon }
-            >
-              Волосы
-            </GradientButton>
-          </Grid>
-        </MarginContainer>
-        <Flex
-          direction='column'
-          gap='10px'
-        >
-          <Hair state={ formState } setState={ setFormState }/>
-        </Flex>
-      </Container>
-    </CharacterCreatorWrapper>
+              <div>
+                <Title variant='title'>
+                  СОЗДАНИЕ <br/> ПЕРСОНАЖА
+                </Title>
+                <MarginContainer
+                  top='23px'
+                  bottom='23px'
+                >
+                  <Grid
+                    columns={ 5 }
+                    columnGap={ 7 }
+                  >
+                    <GradientButton
+                      active={ activeTab === 'dna' }
+                      Icon={ DnaIcon }
+                      onClick={ () => changeTab('dna', 0) }
+                    >
+                      ДНК
+                    </GradientButton>
+                    <GradientButton
+                      active={ activeTab === 'skin' }
+                      Icon={ AppearanceIcon }
+                      onClick={ () => changeTab('skin', 1) }
+                    >
+                      Облик
+                    </GradientButton>
+                    <GradientButton
+                      active={ activeTab === 'face' }
+                      Icon={ FaceIcon }
+                      onClick={ () => changeTab('face', 2) }
+                    >
+                      Лицо
+                    </GradientButton>
+                    <GradientButton
+                      active={ activeTab === 'facialStructure' }
+                      Icon={ FacialStructureIcon }
+                      onClick={ () => changeTab('facialStructure', 3) }
+                    >
+                      Строение
+                      лица
+                    </GradientButton>
+                    <GradientButton
+                      active={ activeTab === 'hair' }
+                      Icon={ HairIcon }
+                      onClick={ () => changeTab('hair', 4) }
+                    >
+                      Волосы
+                    </GradientButton>
+                  </Grid>
+                </MarginContainer>
+                <Flex
+                  direction='column'
+                  gap='10px'
+                >
+                  { FORM_PAGES_MAP[activeTab] }
+                </Flex>
+              </div>
+              <Flex gap='13px'>
+                <Button
+                  variant='danger'
+                  fullWidth
+                >
+                  СОХРАНИТЬ ПЕРСОНАЖА
+                </Button>
+                <RandomizeButton
+                  justifyContent='center'
+                  alignItems='center'
+                  onClick={ () => randomizeFormData() }
+                >
+                  <RandomizeIcon/>
+                </RandomizeButton>
+              </Flex>
+            </Container>
+          </CharacterCreatorWrapper>
+        )
+      }
+    </>
   );
 };
