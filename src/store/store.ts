3d3fc9366f5815eed3c45fdabecbe9d4d62eb68f
uvanov <kirill.ivanov000@gmail.com>
2022-05-12 19:30:35 +0300

    Create ChatSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,12 +6,14 @@ import authenticationSlice from './slices/AuthenticationSlice';
 import selectCharacterSlice from './slices/SelectCharacterSlice';
 import characterCreatorSlice from './slices/CharacterCreatorSlice';
 import spawnSelectSlice from './slices/SpawnSelectSlice';
+import chatSlice from './slices/ChatSlice';
 
 const rootReducer = combineReducers({
   authentication: authenticationSlice,
   selectCharacter: selectCharacterSlice,
   characterCreator: characterCreatorSlice,
-  spawnSelectSlice: spawnSelectSlice
+  spawnSelectSlice: spawnSelectSlice,
+  chatSlice: chatSlice
 });
 
 export const store = configureStore({
