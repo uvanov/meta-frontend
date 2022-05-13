af932869ce9bf0cfec0d3b1738e978cbb168ace5
uvanov <kirill.ivanov000@gmail.com>
2022-05-13 15:26:01 +0300

    ChatInput Layout / Logic wip

--- src/components/pages/chat/components/ChatInput.jsx
+++ src/components/pages/chat/components/ChatInput.jsx
@@ -1,34 +1,85 @@
 // Import modules
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import styled from '@emotion/styled';
 
 // Local modules
 import {
-  MarginContainer,
   Flex,
-  Typography
+  MarginContainer
 } from '@ui/index';
+import { addHexAlpha } from '@lib/utils';
+import { ChatContext } from '../Chat';
+
+// Assets
+import { ReactComponent as SendIcon } from '@images/chat/send-icon.svg';
 
 // Styled Components
-const InputLabel = styled(Typography)`
-  content: 'Enter';
-  display: inline-block;
-  font-size: ${({ theme }) => theme.typography.size.extrasmall};
-`
+const ChatInputWrapper = styled(Flex)`
+  padding: 16px;
+  background-color: ${ ({ background }) => addHexAlpha(background, 0.7) };
+`;
+
+const StyledChatInput = styled.input`
+  width: 100%;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  font-weight: 600;
+  color: ${ ({ theme }) => theme.palette.white };
+  font-size: ${ ({ theme }) => theme.typography.size.small };
+  
+  &::placeholder {
+    font-weight: normal;
+    color: ${ ({ theme }) => addHexAlpha(theme.palette.white, 0.5) };
+  }
+`;
+
+const StyledSendIcon = styled(SendIcon)`
+  cursor: pointer;
+`;
+
+// Constants
+const COMMAND_REGEX = /^\/[\w\s]+/;
+const MESSAGE_REGEX = /<{(#([a-f0-9]{6})})(.*?!*)>/gi;
 
 // Exports
 export const ChatInput = () => {
-  return (
-    <MarginContainer
-      right='27px'
-      left='27px'
-    >
-      <Flex
-        direction='column'
-        gap='8px'
-      >
+  const {
+    chatTheme,
+    state
+  } = useContext(ChatContext);
+  const [message, setMessage] = useState('');
 
-      </Flex>
-    </MarginContainer>
+  const onMessageSend = () => {
+
+  };
+
+  return (
+    <>
+      {
+        state.chatFocused && (
+          <MarginContainer
+            top='27px'
+            right='35px'
+            left='35px'
+            bottom='20px'
+          >
+            <ChatInputWrapper
+              alignItems='center'
+              gap='13px'
+              background={ chatTheme.HEADER }
+            >
+              <StyledChatInput
+                value={ message }
+                onChange={ event => setMessage(event.target.value) }
+                placeholder='Сообщение...'
+                spellCheck='false'
+              />
+              <StyledSendIcon/>
+            </ChatInputWrapper>
+          </MarginContainer>
+        )
+      }
+    </>
   );
 };
