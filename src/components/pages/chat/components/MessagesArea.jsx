a51713e127a24fc6f7ee0a54b5565d482a9aa6a1
uvanov <kirill.ivanov000@gmail.com>
2022-05-13 13:44:46 +0300

    Format coming messages

--- src/components/pages/chat/components/MessagesArea.jsx
+++ src/components/pages/chat/components/MessagesArea.jsx
@@ -1,5 +1,5 @@
 // Import modules
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
 // Local modules
@@ -8,6 +8,7 @@ import {
   MarginContainer, Flex
 } from '@ui/index';
 import { addHexAlpha } from '@lib/utils';
+import { useAppSelector } from '@hooks/state';
 
 // Styled Components
 const StyledMessagesArea = styled(Flex)`
@@ -28,12 +29,62 @@ const StyledMessagesArea = styled(Flex)`
   }
 `
 
+// Constants
+const COLOR_REGEX = /<{(#([a-f0-9]{6})})(.*?)>/gi;
+const BOLD_REGEX = /%(.*?)%/ig;
+const COMMAND_REGEX = /^\/[\w\s]+/;
+
 // Exports
-export const MessagesArea = ({ messages }) => {
+export const MessagesArea = () => {
+
+  const messages = useAppSelector(state => state.chatSlice.messages);
+  const [preparedMessageList, setPreparedMessageList] = useState([]);
+
+  const formatColor = (message) => {
+
+    if(!message.match(COLOR_REGEX)){
+      return formatBold(message);
+    }
+
+    while(message.match(COLOR_REGEX)){
+      const messageExec = COLOR_REGEX.exec(message);
+      console.log(messageExec);
+      message = message.replace(
+        messageExec[0],
+        `<span style="color: #${ messageExec[2] }">${ formatBold(messageExec[3]) }</span>`
+      );
+    }
+
+    return message;
+  };
+
+  const formatBold = (message) => {
+    if(!message.match(BOLD_REGEX)){
+      return message;
+    }
+
+    while (message.match(BOLD_REGEX)) {
+      const messageExec = BOLD_REGEX.exec(message);
+      message = message.replace(
+        messageExec[0],
+        `<strong>${ messageExec[1] }</strong>`
+      );
+    }
+
+    return message;
+  }
+
+  useEffect(() => {
+    const newMessage = messages[messages.length - 1].message;
+    const formattedMessage = formatColor(newMessage);
+
+    setPreparedMessageList([...preparedMessageList, formattedMessage]);
+  }, [messages]);
+
   return (
     <MarginContainer
-      right='27px'
-      left='27px'
+      right='35px'
+      left='35px'
       style={ { height: '100%' } }
     >
       <StyledMessagesArea
@@ -41,9 +92,14 @@ export const MessagesArea = ({ messages }) => {
         gap='16px'
       >
         {
-          messages && messages.map(message => (
-            <Typography variant='small' color='black'>
-              { message }
+          preparedMessageList.map(message => (
+            <Typography
+              key={ Date.now() }
+              variant='small'
+              color='black'
+              dangerouslySetInnerHTML={ { __html: message } }
+            >
+
             </Typography>
           ))
         }
