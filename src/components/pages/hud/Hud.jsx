15ddb5885d11b2091b44c99828277fa9ec3dbc4d
uvanov <kirill.ivanov000@gmail.com>
2022-05-17 14:59:39 +0300

    Hud Layout wip

--- src/components/pages/hud/Hud.jsx
+++ src/components/pages/hud/Hud.jsx
@@ -4,13 +4,23 @@ import styled from '@emotion/styled';
 
 // Local modules
 import { Header } from './components/Header';
+import { Hints } from './components/Hints';
+import { Information } from '@pages/hud/components/Information';
+
+// Styled Components
+const StyledHud = styled.div`
+  position: relative;
+  height: 100%;
+`
 
 // Exports
 export const Hud = () => {
   return (
-    <div>
+    <StyledHud>
       <Header />
-    </div>
+      <Hints fromBottom='25%' />
+      <Information fromLeft='20%' />
+    </StyledHud>
   );
 };
 
@@ -25,7 +35,10 @@ export const Hud = () => {
 *
 * Information:
 *   Жизни, Голод, Жажда, Наличные (предусмотреть большие значения)
-*   Бензин, Состояние авто, Состояние дверей ( locked / unlocked ), Состояние фар ( on / off  ),
-*   Текущая скорость авто
+*
+* Car
+*   Отображение спидометра всего (on off), скорость, максимальная скорость нынешнего авто
+*   Бензин, Состояние авто, Состояние дверей ( locked / unlocked ), Состояние фар ( on / off  ), поворотники
+*
 *   ( так же реализовать возможность скрывать каждый из элементов по отдельности )
 * */
