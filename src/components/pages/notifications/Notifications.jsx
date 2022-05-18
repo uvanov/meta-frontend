71c54e68e927254102d8f1ef97c74964dec2987f
uvanov <kirill.ivanov000@gmail.com>
2022-05-18 15:07:39 +0300

    Create Notifications Component wip

---
+++ src/components/pages/notifications/Notifications.jsx
@@ -0,0 +1,12 @@
+// Import modules
+import React from 'react';
+import styled from '@emotion/styled';
+
+// Exports
+export const Notifications = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
